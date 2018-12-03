2bd448689b3a37d0f245a27b7762a3cc72649c30
Rachel Laub <rachellaub@mail.weber.edu>
2018-12-03 15:00:38 +0000

    Final changes

--- rachellaub_exam2_p1.py
+++ rachellaub_exam2_p1.py
@@ -12,8 +12,32 @@ def is_valid_password(password):
     8 char long
     At least one upper and lower case letter
     At least 1 digit
+    :param password: Password entered in by user
     """
-    print(password)
+    counter = 0
+    if(len(password)<8):
+        print("Your password must be at least 8 chars long.")
+    else:
+        counter += 1
+
+    if not re.search("[a-z]",password):
+        print("Your password must have at least 1 lower case letter.")
+    else:
+        counter += 1
+
+    if not re.search("[A-Z]",password):
+        print("Your password must have at least 1 upper case letter.")
+    else:
+        counter += 1
+
+    if not re.search("[0-9]",password):
+        print("Your password must have at least 1 digit.")
+    else:
+        counter += 1
+
+    if counter = 4:
+        print("Your pair of passwords will work")
+
 
 
 def main():
